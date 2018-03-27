a0562f064cf4fa93a8f9aecca7968763df6d7451
Hsieh Shang-Fu <shawnhsieh@Hsiehde-MacBook-Pro.local>
2018-03-26 22:03:54 -0700

    complete map template

--- src/app/app-form/detail-page/detail-map/detail-map.component.ts
+++ src/app/app-form/detail-page/detail-map/detail-map.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import { } from 'googlemaps';
 
 @Component({
   selector: 'app-detail-map',
@@ -7,10 +8,23 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class DetailMapComponent implements OnInit {
   @Input() place: any;
+  detailname
+  detailaddress;
+
+  public latitude: number;
+  public longitude: number;
+  public zoom: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.detailname = this.place.name;
+    this.detailaddress = this.place.formatted_address;
+
+    this.zoom = 4;
+    this.latitude = 39.8282;
+    this.longitude = -98.5795;
+
   }
 
 }
