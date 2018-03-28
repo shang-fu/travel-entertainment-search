2fd6e746c4dd3956bcf651203bbdf0d52fdbec51
Hsieh Shang-Fu <shawnhsieh@Hsiehde-MacBook-Pro.local>
2018-03-28 16:07:12 -0700

    ready for big change

--- src/app/app-form/detail-page/detail-info/detail-info.component.ts
+++ src/app/app-form/detail-page/detail-info/detail-info.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 declare var jquery:any;
 declare var $ :any;
 
@@ -8,81 +8,100 @@ declare var $ :any;
   styleUrls: ['./detail-info.component.css']
 })
 export class DetailInfoComponent implements OnInit {
-  @Input() place: any;
-  detailaddress;
-  detailphone;
-  detailprice;
+  place: any;
+  @Output() sendPlace = new EventEmitter<any>();
+  @Input() placeid: string;
+  @ViewChild('dummy') dummymap: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
-    if (this.place) {
-      if (this.place.formatted_address) {
-        this.detailaddress = this.place.formatted_address;
-      }
-      if (this.place.international_phone_number) {
-        this.detailphone = this.place.international_phone_number;
-      }
 
-      if (this.place.price_level) {
-        if (this.place.price_level == 1) {
-          this.detailprice = '$';
-        } else if (this.place.price_level == 2) {
-          this.detailprice = '$$';
-        } else if (this.place.price_level == 3) {
-          this.detailprice = '$$$';
-        } else if (this.place.price_level == 4) {
-          this.detailprice = '$$$$';
+
+    let service = new google.maps.places.PlacesService(new google.maps.Map(this.dummymap.nativeElement));
+    // console.log(service);
+
+    service.getDetails({
+      placeId: this.placeid
+    }, (place, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        console.log(place);
+        this.place = place;
+        this.sendPlace.emit(this.place);
+
+
+        if (this.place) {
+          if (this.place.formatted_address) {
+            $('#detailaddress').text(this.place.formatted_address);
+          }
+          if (this.place.international_phone_number) {
+            $('#detailphone').text(this.place.international_phone_number);
+          }
+
+          if (this.place.price_level) {
+            if (this.place.price_level == 1) {
+              $('#detailprice').text('$');
+            } else if (this.place.price_level == 2) {
+              $('#detailprice').text('$');
+            } else if (this.place.price_level == 3) {
+              $('#detailprice').text('$');
+            } else if (this.place.price_level == 4) {
+              $('#detailprice').text('$');
+            }
+          }
+
+          if (this.place.rating) {
+            let rate = this.place.rating;
+            if (rate == 1) {
+              $('#detailrating').prepend(`1.0 `);
+              $('#detail-star').addClass('stars-100');
+            } else if (rate == 2) {
+              $('#detailrating').prepend(`2.0 <span style="color: rgb(219, 115, 53)">★</span>`);
+              $('#detail-star').addClass('stars-100');
+            } else if (rate == 3) {
+              $('#detailrating').prepend(`3.0 <span style="color: rgb(219, 115, 53)">★★</span>`);
+              $('#detail-star').addClass('stars-100');
+            } else if (rate == 4) {
+              $('#detailrating').prepend(`4.0 <span style="color: rgb(219, 115, 53)">★★★</span>`);
+              $('#detail-star').addClass('stars-100');
+            } else if (rate == 5) {
+              $('#detailrating').prepend(`4.0 <span style="color: rgb(219, 115, 53)">★★★★</span>`);
+              $('#detail-star').addClass('stars-100');
+            } else if (0 <= rate && rate < 1) {
+              $('#detailrating').prepend(`${rate} `);
+              $('#detail-star').addClass(`stars-${rate * 100}`);
+            } else if (1 < rate && rate < 2) {
+              $('#detailrating').prepend(`${rate} <span style="color: rgb(219, 115, 53)">★</span>`);
+              $('#detail-star').addClass(`stars-${Math.round((rate - 1) * 100)}`);
+            } else if (2 < rate && rate < 3) {
+              $('#detailrating').prepend(`${rate} <span style="color: rgb(219, 115, 53)">★★</span>`);
+              $('#detail-star').addClass(`stars-${Math.round((rate - 2) * 100)}`);
+            } else if (3 < rate && rate < 4) {
+              $('#detailrating').prepend(`${rate} <span style="color: rgb(219, 115, 53)">★★★</span>`);
+              $('#detail-star').addClass(`stars-${Math.round((rate - 3) * 100)}`);
+            } else if (4 < rate && rate < 5) {
+              $('#detailrating').prepend(`${rate} <span style="color: rgb(219, 115, 53)">★★★★</span>`);
+              $('#detail-star').addClass(`stars-${Math.round((rate - 4) * 100)}`);
+            }
+
+          }
+
+          if (this.place.url) {
+            $('#detailgooglepage').html(`<a  href="${this.place.url}" target="_blank">${this.place.url}</a>`);
+          }
+          if (this.place.website) {
+            $('#detailwebsite').html(`<a  href="${this.place.website}" target="_blank">${this.place.website}</a>`);
+          }
+
+
         }
-      }
 
-      if (this.place.rating) {
-        let rate = this.place.rating;
-        if (rate == 1) {
-          $('#detailrating').prepend(`1.0 `);
-          $('#detail-star').addClass('stars-100');
-        } else if (rate == 2) {
-          $('#detailrating').prepend(`2.0 <span style="color: rgb(219, 115, 53)">★</span>`);
-          $('#detail-star').addClass('stars-100');
-        } else if (rate == 3) {
-          $('#detailrating').prepend(`3.0 <span style="color: rgb(219, 115, 53)">★★</span>`);
-          $('#detail-star').addClass('stars-100');
-        } else if (rate == 4) {
-          $('#detailrating').prepend(`4.0 <span style="color: rgb(219, 115, 53)">★★★</span>`);
-          $('#detail-star').addClass('stars-100');
-        } else if (rate == 5) {
-          $('#detailrating').prepend(`4.0 <span style="color: rgb(219, 115, 53)">★★★★</span>`);
-          $('#detail-star').addClass('stars-100');
-        } else if (0 <= rate && rate < 1 ) {
-          $('#detailrating').prepend(`${rate} `);
-          $('#detail-star').addClass(`stars-${rate * 100}`);
-        } else if (1 < rate && rate < 2 ) {
-          $('#detailrating').prepend(`${rate} <span style="color: rgb(219, 115, 53)">★</span>`);
-          $('#detail-star').addClass(`stars-${Math.round((rate - 1) * 100)}`);
-        } else if (2 < rate && rate < 3 ) {
-          $('#detailrating').prepend(`${rate} <span style="color: rgb(219, 115, 53)">★★</span>`);
-          $('#detail-star').addClass(`stars-${Math.round((rate - 2) * 100)}`);
-        } else if (3 < rate && rate < 4 ) {
-          $('#detailrating').prepend(`${rate} <span style="color: rgb(219, 115, 53)">★★★</span>`);
-          $('#detail-star').addClass(`stars-${Math.round((rate - 3) * 100)}`);
-        } else if (4 < rate && rate < 5 ) {
-          $('#detailrating').prepend(`${rate} <span style="color: rgb(219, 115, 53)">★★★★</span>`);
-          $('#detail-star').addClass(`stars-${Math.round((rate - 4) * 100)}`);
-        }
-
-      }
-
-      if (this.place.url) {
-        $('#detailgooglepage').html(`<a  href="${this.place.url}" target="_blank">${this.place.url}</a>`);
-      }
-      if (this.place.website) {
-        $('#detailwebsite').html(`<a  href="${this.place.website}" target="_blank">${this.place.website}</a>`);
-      }
 
 
 
 
-    }
+      }
+    });
   }
 
 }
--- src/app/app-form/detail-page/detail-page.component.ts
+++ src/app/app-form/detail-page/detail-page.component.ts
@@ -17,7 +17,7 @@ export class DetailPageComponent implements OnInit {
   @Input() detailLng: number;
   @Input() currentLat: number;
   @Input() currentLng: number;
-  @ViewChild('dummy') dummymap: ElementRef;
+
 
   placeheader: string;
   place: any;
@@ -25,95 +25,6 @@ export class DetailPageComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-
-    console.log(this.placeid);
-
-
-    let service = new google.maps.places.PlacesService(new google.maps.Map(this.dummymap.nativeElement));
-    // console.log(service);
-
-    service.getDetails({
-      placeId: this.placeid
-    }, (place, status) => {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        console.log(place);
-        this.place = place;
-
-        if (place.name) {
-          $('#detailheader').text(place.name);
-        }
-
-        if (place.formatted_address) {
-          $('#detailaddress').text(place.formatted_address);
-        }
-
-        if (place.international_phone_number) {
-          $('#detailphone').text(place.international_phone_number);
-        }
-
-        if (place.price_level) {
-          if (place.price_level == 1) {
-            $('#detailprice').text('$');
-          } else if (place.price_level == 2) {
-            $('#detailprice').text('$$');
-          } else if (place.price_level == 3) {
-            $('#detailprice').text('$$$');
-          } else if (place.price_level == 4) {
-            $('#detailprice').text('$$$$');
-          }
-        }
-
-        if (place.rating) {
-          let rate = place.rating;
-          if (rate == 1) {
-            $('#detailrating').prepend(`1.0 `);
-            $('#detail-star').addClass('stars-100');
-          } else if (rate == 2) {
-            $('#detailrating').prepend(`2.0 <span style="color: rgb(219, 115, 53)">★</span>`);
-            $('#detail-star').addClass('stars-100');
-          } else if (rate == 3) {
-            $('#detailrating').prepend(`3.0 <span style="color: rgb(219, 115, 53)">★★</span>`);
-            $('#detail-star').addClass('stars-100');
-          } else if (rate == 4) {
-            $('#detailrating').prepend(`4.0 <span style="color: rgb(219, 115, 53)">★★★</span>`);
-            $('#detail-star').addClass('stars-100');
-          } else if (rate == 5) {
-            $('#detailrating').prepend(`4.0 <span style="color: rgb(219, 115, 53)">★★★★</span>`);
-            $('#detail-star').addClass('stars-100');
-          } else if (0 <= rate && rate < 1 ) {
-            $('#detailrating').prepend(`${rate} `);
-            $('#detail-star').addClass(`stars-${rate * 100}`);
-          } else if (1 < rate && rate < 2 ) {
-            $('#detailrating').prepend(`${rate} <span style="color: rgb(219, 115, 53)">★</span>`);
-            $('#detail-star').addClass(`stars-${Math.round((rate - 1) * 100)}`);
-          } else if (2 < rate && rate < 3 ) {
-            $('#detailrating').prepend(`${rate} <span style="color: rgb(219, 115, 53)">★★</span>`);
-            $('#detail-star').addClass(`stars-${Math.round((rate - 2) * 100)}`);
-          } else if (3 < rate && rate < 4 ) {
-            $('#detailrating').prepend(`${rate} <span style="color: rgb(219, 115, 53)">★★★</span>`);
-            $('#detail-star').addClass(`stars-${Math.round((rate - 3) * 100)}`);
-          } else if (4 < rate && rate < 5 ) {
-            $('#detailrating').prepend(`${rate} <span style="color: rgb(219, 115, 53)">★★★★</span>`);
-            $('#detail-star').addClass(`stars-${Math.round((rate - 4) * 100)}`);
-          }
-        }
-
-        if (place.url) {
-          $('#detailgooglepage').html(`<a  href="${place.url}" target="_blank">${place.url}</a>`);
-        }
-        if (place.website) {
-          $('#detailwebsite').html(`<a  href="${place.website}" target="_blank">${place.website}</a>`);
-        }
-
-
-
-        return;
-      }
-
-    });
-
-
-
   }
 
 
@@ -125,4 +36,7 @@ export class DetailPageComponent implements OnInit {
     this.detailSelected = $event;
   }
 
+  onSendPlace($event) {
+    this.place = $event;
+  }
 }
