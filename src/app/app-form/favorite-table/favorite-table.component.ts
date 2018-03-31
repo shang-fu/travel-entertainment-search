c5d61a9d43952cf7e2e2c27f88a832d7b8dad7da
Hsieh Shang-Fu <shawnhsieh@Hsiehde-MacBook-Pro.local>
2018-03-31 16:25:13 -0700

    fav page

--- src/app/app-form/favorite-table/favorite-table.component.ts
+++ src/app/app-form/favorite-table/favorite-table.component.ts
@@ -12,9 +12,21 @@ export class FavoriteTableComponent implements OnInit {
   @Output() localStorageChangeEvent = new EventEmitter<any>();
   @Output() hasDetail = new EventEmitter<any>();
 
+  currentPage;
+  hasNextPage;
+  hasPrevPage;
+  startIndex;
+
   constructor() { }
 
   ngOnInit() {
+    this.startIndex = 0;
+    this.hasPrevPage = false;
+    if (this.localStorageFormattedList.length > 20) {
+      this.hasNextPage = true;
+    } else {
+      this.hasNextPage = false;
+    }
   }
 
   getDetail(placeid: string, icon: string, name: string, address: string, lat: number, lng: number) {
@@ -31,6 +43,51 @@ export class FavoriteTableComponent implements OnInit {
   onFavRemove(placeid: string) {
     localStorage.removeItem(placeid);
     this.localStorageChangeEvent.emit();
+
+    setTimeout(() => {
+      if (this.startIndex >= this.localStorageFormattedList.length) {
+        this.startIndex -= 20;
+        if (this.startIndex <= 0 ) {
+          this.hasPrevPage = false;
+        } else {
+          this.hasPrevPage = true;
+        }
+        this.hasNextPage = false;
+      } else {
+        if (this.startIndex + 20 >= this.localStorageFormattedList.length) {
+          this.hasNextPage = false;
+        } else {
+          this.hasNextPage = true;
+        }
+      }
+    }, 300);
+  }
+
+  onShowNextPage() {
+    this.startIndex += 20;
+    this.hasPrevPage = true;
+
+    if (this.startIndex + 20 >= this.localStorageFormattedList.length) {
+      this.hasNextPage = false;
+    } else {
+      this.hasNextPage = true;
+    }
+  }
+
+  onShowPrevPage() {
+    this.startIndex -= 20;
+    if (this.startIndex <= 0 ) {
+      this.hasPrevPage = false;
+    } else {
+      this.hasPrevPage = true;
+    }
+
+    if (this.startIndex + 20 >= this.localStorageFormattedList.length) {
+      this.hasNextPage = false;
+    } else {
+      this.hasNextPage = true;
+    }
+
   }
 
 }
